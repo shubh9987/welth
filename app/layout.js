035dc469dbe3cf7db101296f5df5dd14ba4b8934
Shreyas Tyagi <dontyagicorleone9@gmail.com>
2025-03-29 00:07:51 +0530

    Updated Layout

--- app/layout.js
+++ app/layout.js
@@ -3,27 +3,28 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from 'sonner';
-import { useEffect } from "react";
 
-const inter = Inter({ subset: ["latin"] });
+
+
+const inter = Inter({ subsets: ["latin", "latin-ext"] });
+
 
 export const metadata = {
   title: "Welth",
   description: "One stop Finance Platform",
 };
-
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    document.body.classList.remove("vsc-initialized");
-  }, []);
-
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={"$(inter.classname)"}>
+        <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+        </head>
+        <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
+
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with💖 by Shreyas Tyagi.</p>
@@ -34,3 +35,5 @@ export default function RootLayout({ children }) {
     </ClerkProvider>
   );
 }
+
+  
