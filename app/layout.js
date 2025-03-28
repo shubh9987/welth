25f8284af9672ba6d67eac5620a0e118a700caf4
Shreyas Tyagi <dontyagicorleone9@gmail.com>
2025-03-28 23:00:42 +0530

    Updated RootLayout to remove vsc-initialized class

--- app/layout.js
+++ app/layout.js
@@ -3,8 +3,9 @@ import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from 'sonner';
+import { useEffect } from "react";
 
-const inter = Inter({subset: ["latin"] });
+const inter = Inter({ subset: ["latin"] });
 
 export const metadata = {
   title: "Welth",
@@ -12,26 +13,24 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    document.body.classList.remove("vsc-initialized");
+  }, []);
+
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-      
-        className={"$(inter.classname)"}>
-      
-      <Header /> 
-      <main className="min-h-screen">{children}</main>
-        <Toaster richColors />
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>
-              Made with💖 by Shreyas Tyagi.
-
-            </p>
-          </div>
-        </footer>
-      </body>
-    </html>
+      <html lang="en">
+        <body className={"$(inter.classname)"}>
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with💖 by Shreyas Tyagi.</p>
+            </div>
+          </footer>
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
